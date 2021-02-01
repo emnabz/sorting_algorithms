4cefd134078f20759cfb69a378755fa6f8cc0824
emnabz <1947@holbertonschool.com>
2021-02-01 15:07:28 +0100

    task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,18 +9,18 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 int n;
 if (size < 2)
-  return;
+return;
 for (i = 0; i < size - 1; i++)
-  {
-    for (j = 0; j < size - i - 1; j++)
-      {
-	if (array[j] > array[j + 1])
-	  {
-	    n = array[j];
-	    array[j] = array[j + 1];
-	    array[j + 1] = n;
-	    print_array(array, size);
-	  }
-      }
-  }
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+n = array[j];
+array[j] = array[j + 1];
+array[j + 1] = n;
+print_array(array, size);
+}
+}
+}
 }
